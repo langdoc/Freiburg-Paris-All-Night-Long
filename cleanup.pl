0e34827ce97f518ad32822e94840f8a978bdabb4
Clément Plancq <clement.plancq@ens.fr>
2017-01-09 11:45:14 +0100

    New cleanup, filters out russian words (from aspell list)

--- cleanup.pl
+++ cleanup.pl
@@ -13,14 +13,37 @@ binmode(STDOUT, ":utf8");
 my $usage = "perl $0 input_file\n";
 die $usage unless @ARGV == 1 && -f $ARGV[0];
 
+my $ASPELL_FILE = "./ru-aspell-wordlist.txt";
+
+## reads russian from aspell list
+## args : aspell file (ru-aspell-wordlist.txtru-aspell-wordlist.txt)
+## returns a hash of words (key: word, value: none). hash if used for lookup
+sub russian_words{
+    my $russian_file = shift;
+    my %words_ru;
+    open(FILE, "<:encoding(UTF8)", $russian_file) || die $!;
+    while(<FILE>){
+	my $word = $_;
+	chomp $word;
+	$word =~ s/(\p{L}+)(\/[A-Za-z])?/$1/;
+	$words_ru{$word} = "";
+    }
+    close FILE;
+
+    return %words_ru;
+}
+
+my %words_ru = russian_words($ASPELL_FILE);
 
 open(FILE, "<:encoding(UTF8)", $ARGV[0]) || die $!;
 while(<FILE>){
     my @cols = split(/\t/, $_);
     if($cols[0] =~ /\p{Script:Cyrillic}+/){
-      $cols[0] =~ s/^\P{L}//g;
-      $cols[0] =~ s/\P{L}$//g;
-      print "$cols[0]\t$cols[1]";
-  }
+	$cols[0] =~ s/^\P{L}//g;
+	$cols[0] =~ s/\P{L}$//g;
+	unless(exists($words_ru{$cols[0]})){
+	    print "$cols[0]\t$cols[1]";
+	}
+    }
 }
 close(FILE);
